edc2fef949c9d3e0c983a0e2b823f9dd8fbb6de7
speedprogs_de <speedprogs_de@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-09 13:58:04 +0000

    fixed indention
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/terfe/branches/ter_fe2@61720 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/ExtensionController.php
+++ Classes/Controller/ExtensionController.php
@@ -88,6 +88,7 @@
 			$this->extensionRepository->setShowInsecure($this->isReviewer());
 		}
 
+
 		/**
 		 * Checks wether FE User is reviewer or not
 		 *
@@ -115,41 +116,42 @@
 			$this->searchAction();
 		}
 
-        /**
-         * search action (USER_INT)
-         *
-         * @param array $search Search params for extension list
-         * @param array $restoreSearch Restore last search from session
-         * @return void
-         * @dontvalidate $search
-         */
-        public function searchAction(array $search = array(), $restoreSearch = FALSE) {
-            // Get extension list
-            if (!empty($this->settings['show']['extensionSearch'])) {
-                $this->view->assign('extensions', $this->getExtensions($search, $restoreSearch));
-                $this->view->assign('search',     $search);
-            } else {
-                $this->view->assign('extensions', $this->extensionRepository->findAll());
-            }
 
-            // Get all categories
-            if (!empty($this->settings['show']['categoryOverview'])) {
-                $categories = $this->categoryRepository->findAll();
-                $this->view->assign('categories', $categories);
-            }
+		/**
+		 * search action (USER_INT)
+		 *
+		 * @param array $search Search params for extension list
+		 * @param array $restoreSearch Restore last search from session
+		 * @return void
+		 * @dontvalidate $search
+		 */
+		public function searchAction(array $search = array(), $restoreSearch = FALSE) {
+			// Get extension list
+			if (!empty($this->settings['show']['extensionSearch'])) {
+				$this->view->assign('extensions', $this->getExtensions($search, $restoreSearch));
+				$this->view->assign('search', $search);
+			} else {
+				$this->view->assign('extensions', $this->extensionRepository->findAll());
+			}
 
-            // Get all tags
-            if (!empty($this->settings['show']['tagOverview'])) {
-                $tags = $this->tagRepository->findAll();
-                $this->view->assign('tags', $tags);
-            }
+			// Get all categories
+			if (!empty($this->settings['show']['categoryOverview'])) {
+				$categories = $this->categoryRepository->findAll();
+				$this->view->assign('categories', $categories);
+			}
 
-            // Get authors
-            if (!empty($this->settings['show']['authorOverview'])) {
-                $authors = $this->authorRepository->findByLatestExtensionVersion();
-                $this->view->assign('authors', $authors);
-            }
-        }
+			// Get all tags
+			if (!empty($this->settings['show']['tagOverview'])) {
+				$tags = $this->tagRepository->findAll();
+				$this->view->assign('tags', $tags);
+			}
+
+			// Get authors
+			if (!empty($this->settings['show']['authorOverview'])) {
+				$authors = $this->authorRepository->findByLatestExtensionVersion();
+				$this->view->assign('authors', $authors);
+			}
+		}
 
 
 		/**
